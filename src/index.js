8627638037a8e5148863ae27fc00c227c939b127
bruno <bruno.3av@gmail.com>
2022-02-17 15:39:20 -0300

    added pwa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import Calculator from './main/Calculator'; //Troque o componente 'App' por 'Calculator'
-import reportWebVitals from './reportWebVitals';
-
+import "./index.css";
+import Calculator from "./main/Calculator"; //Troque o componente 'App' por 'Calculator'
 
 ReactDOM.render(
   <div>
     <h1>Calculadora</h1>
     <Calculator />
-  </div>
-  , document.getElementById('root')
+  </div>,
+  document.getElementById("root")
 );
-
-reportWebVitals();
